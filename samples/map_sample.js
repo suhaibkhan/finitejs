ed292b04eaa0cd09d50ddf7925d98288413c1db6
Suhaib Khan <suhaibklm@gmail.com>
2015-09-28 23:31:44 +0530

    Changed Map sample file.

--- samples/map_sample.js
+++ samples/map_sample.js
@@ -1,5 +1,17 @@
 
 
 var m = new Map;
+
 m.put('s', 123);
+m.put('k', 321);
+
+print(m.size());
 print(m.get('s'));
+
+m.delete('s');
+
+print(m.size());
+print(m.get('s'));
+print(m.get('k'));
+
+print(m.toString());
