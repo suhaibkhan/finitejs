7951c1a41d04df22240d56c6270a0231650f5311
Suhaib Khan <suhaibklm@gmail.com>
2015-10-04 20:15:24 +0530

    Added javadoc comments.

--- src/main/java/com/finitejs/modules/core/ConsoleUtils.java
+++ src/main/java/com/finitejs/modules/core/ConsoleUtils.java
@@ -5,37 +5,81 @@ import java.io.InputStreamReader;
 
 /**
  * Utility class for console I/O.
- * @author Suhaib Khan
- *
  */
 public class ConsoleUtils {
 
+	/**
+	 * Obtains console input reader.
+	 * Used in REPL mode.
+	 * 
+	 * @return input reader
+	 */
 	public static BufferedReader getInputReader(){
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		return reader;
 	}
 	
+	/**
+	 * Prints string representation of an object to console.
+	 * 
+	 * @param object  object to print
+	 */
 	public static void print(Object object){
 		System.out.print(object);
 	}
 	
+	/**
+	 * Prints string representation of an object to 
+	 * console and terminates with a line break.
+	 * 
+	 * @param object  object to print
+	 */
 	public static void println(Object object){
 		System.out.println(object);
 	}
 	
+	/**
+	 * Prints a formatted string to console using 
+	 * the given format and arguments.
+	 * 
+	 * @param format  format specifier
+	 * @param args  arguments in format
+	 * 
+	 * @see String.format
+	 */
 	public static void printf(String format, Object... args){
 		String str = String.format(format, args);
 		System.out.print(str);
 	}
 	
+	/**
+	 * Prints string representation of an object to error console/stream.
+	 * 
+	 * @param object  object to print
+	 */
 	public static void error(Object error){
 		System.err.print(error);
 	}
 	
+	/**
+	 * Prints string representation of an object to 
+	 * error console/stream and terminates with a line break.
+	 * 
+	 * @param object  object to print
+	 */
 	public static void errorln(Object error){
 		System.err.println(error);
 	}
 	
+	/**
+	 * Prints a formatted string to error console/stream using 
+	 * the given format and arguments.
+	 * 
+	 * @param format  format specifier
+	 * @param args  arguments in format
+	 * 
+	 * @see String.format
+	 */
 	public static void errorf(String format, Object... args){
 		String error = String.format(format, args);
 		System.err.print(error);
