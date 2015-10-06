51565541ff986d932f50ee9c960029b99f217bb4
Suhaib Khan <suhaibklm@gmail.com>
2015-10-07 01:35:44 +0530

    Added javadoc comments.

--- src/main/java/com/finitejs/modules/read/formatter/RegExFormatter.java
+++ src/main/java/com/finitejs/modules/read/formatter/RegExFormatter.java
@@ -4,11 +4,34 @@ import java.util.regex.Pattern;
 
 import com.finitejs.modules.read.InputFormatter;
 
+/**
+ * 
+ * Input formatter that formats and returns values based on regular expressions.
+ * <p>
+ * Example :
+ * <pre>
+ * InputFormatter formatter = 
+ * 		new RegExFormatter("^(\\d{4})-(\\d{1,2})-(\\d{1,2})$", "$3-$2-$1 00:00:00");
+ * </pre>
+ * Above {@code formatter} can be used to convert value {@code 2014-04-14} to {@code 14-04-2014 00:00:00}.
+ * </p>
+ * 
+ */
 public class RegExFormatter implements InputFormatter{
 
+	/** Compiled RegEx pattern used to format. */
 	private Pattern pattern;
+	
+	/** Replacement string */
 	private String replacement;
 	
+	/**
+	 * Creates a new {@code RegExFormatter} based on 
+	 * a RegEx pattern and replacement.
+	 * 
+	 * @param regex  regular expression to use
+	 * @param replacement  replacement string
+	 */
 	public RegExFormatter(String regex, String replacement){
 		this.pattern = Pattern.compile(regex);
 		this.replacement = replacement;
