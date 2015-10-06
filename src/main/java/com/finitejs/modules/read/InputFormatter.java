77158eef78769d5ce22b58ec1776c428cba869d9
Suhaib Khan <suhaibklm@gmail.com>
2015-10-07 01:04:33 +0530

    Added javadoc comments.

--- src/main/java/com/finitejs/modules/read/InputFormatter.java
+++ src/main/java/com/finitejs/modules/read/InputFormatter.java
@@ -2,11 +2,15 @@ package com.finitejs.modules.read;
 
 /**
  * InputFormatter can be used to format reader input before adding to DataTable. 
- * 
- * @author Suhaib Khan
  */
 public interface InputFormatter {
 
-	String format(String inputValue);
+	/**
+	 * Formats the input string before adding to a {@link DataTable}.
+	 * 
+	 * @param inputValue  input string value
+	 * @return output string value after formatting
+	 */
+	public String format(String inputValue);
 	
 }
