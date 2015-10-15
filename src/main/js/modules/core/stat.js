c85a89dcf269d2fd269ad15c7caa6cb2ec106dc7
Renjith G R <grenjithr@gmail.com>
2015-10-16 01:53:42 +0530

    mean now can be called like mean([1,2,3,4]) or mean(1,2,3,4)

--- src/main/js/modules/core/stat.js
+++ src/main/js/modules/core/stat.js
@@ -3,11 +3,13 @@ var _stat = Java.type('com.finitejs.modules.math.stats.Stat');
 var stat = {};
 
 stat.mean = function(values) {
-    return _stat.mean(Java.to(values, "double[]"));
+    return Array.isArray(values) ?
+        _stat.mean(Java.to(values, "double[]")) : _stat.mean(Java.to(arguments, "double[]"));
 };
 
 stat.std = function(values) {
-    return _stat.std(Java.to(values, "double[]"));
+    return Array.isArray(values) ?
+        _stat.std(Java.to(values, "double[]")) : _stat.std(Java.to(arguments, "double[]"));
 };
 
 module.exports = stat;
