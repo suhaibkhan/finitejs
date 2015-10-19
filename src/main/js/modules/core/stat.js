c5b74bde84eaa757aabc742979998cf0fcaf8048
Renjith G R <grenjithr@gmail.com>
2015-10-20 01:48:15 +0530

    Adding jsdocs to stats.js

--- src/main/js/modules/core/stat.js
+++ src/main/js/modules/core/stat.js
@@ -1,14 +1,29 @@
 'use strict';
 
+/**
+ * Module for statistical functions.
+ *
+ * @exports stat
+ */
 var Stat = Java.type('com.finitejs.modules.math.stats.Stat');
 
 var stat = {};
 
+/**
+ * Returns mean of the values.
+ *
+ * @param {...number|number[]} values
+ */
 stat.mean = function(values) {
     return Array.isArray(values) ?
     	Stat.mean(Java.to(values, "double[]")) : Stat.mean(Java.to(arguments, "double[]"));
 };
 
+/**
+ * Return standard deviation of the values.
+ *
+ * @param {...number|number[]} values
+ */
 stat.std = function(values) {
     return Array.isArray(values) ?
     	Stat.std(Java.to(values, "double[]")) : Stat.std(Java.to(arguments, "double[]"));
