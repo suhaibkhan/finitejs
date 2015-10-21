0f78bedcd720716a70396fbbb93964ef4dadcdc0
Renjith G R <grenjithr@gmail.com>
2015-10-21 19:50:01 +0530

    Adding documentation for abs(), conjugate() and negate() in complex.js

--- src/main/js/modules/core/complex.js
+++ src/main/js/modules/core/complex.js
@@ -12,40 +12,53 @@ function Complex(real, imaginary){
 }
 
 /**
- * Access the real part of the complex number
+ * Access the real part of the complex number.
  *
- * @returns {Number} real part of the complex number.
+ * @returns {number} Real part of the complex number.
  */
 Complex.prototype.real = function() {
     return this._complex.getReal();
 };
 
 /**
- * Access the imaginary part of the complex number
+ * Access the imaginary part of the complex number.
  *
- * @returns {Number} imaginary part of the complex number}
+ * @returns {number} Imaginary part of the complex number.
  */
 Complex.prototype.imaginary = function() {
     return this._complex.getImaginary();
 };
 
+/**
+ * Returns the absolute value of this complex number.
+ *
+ * @returns {number} Absolute value of this complex number.
+ */
+Complex.prototype.abs = function() {
+    return this._complex.abs();
+};
+
+/**
+ * Returns the conjugate of this complex number.
+ *
+ * @returns {Complex} Instance of Complex which represents the conjugate.
+ */
+Complex.prototype.conjugate = function() {
+    var conjugate =  this._complex.conjugate();
+    return new Complex(conjugate.getReal(), conjugate.getImaginary());
+};
+
+/**
+ * Returns the negated value of this complex number.
+ *
+ * @returns {Complex} Instance of Complex which represents negated value of this complex number.
+ */
+Complex.prototype.negate = function() {
+    var negated = this._complex.negate();
+    return new Complex(negated.getReal(), negated.getImaginary());
+};
+
 Complex.prototype = {
-
-
-
-    abs: function() {
-        return this._complex.abs();
-    },
-
-    conjugate: function() {
-        var conjugate =  this._complex.conjugate();
-        return new Complex(conjugate.getReal(), conjugate.getImaginary());
-    },
-
-    negate: function() {
-        var negated = this._complex.negate();
-        return new Complex(negated.getReal(), negated.getImaginary());
-    },
 
     add: function(addend) {
         var sum = this._complex.add(
